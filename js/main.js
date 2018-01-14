6a5444d28a28ab422dd8e635ea51d36b7ef21a6e
Myroslav Kravchenko <kravch@gmail.com>
2018-01-14 21:17:34 +0200

    Added several objects with words to learn

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
-//Initial object with words to learn
-let wordsToLearn = {
+//Initial objects with words to learn
+let weekDays = {
     понедельник:'Monday',
     вторник:'Tuesday',
     среда:'Wednesday',
     четверг:'Thursday',
     пятница:'Friday',
     суббота:'Saturday',
-    воскресень:'Sunday'
+    воскресенье:'Sunday'
+};
+
+let numbersElevenTwenty = {
+    одинадцать:'eleven',
+    двенадцать:'twelve',
+    тринадцать:'thirteen',
+    четырнадцать:'fourteen',
+    пятнадцать:'fifteen',
+    шестнадцать:'sixteen',
+    семнадцать:'seventeen',
+    восемнадцать:'eighteen',
+    девятнадцать:'nineteen',
+    двадцать:'twenty'
 };
 
 //array for shuffled keys, needs to be global
@@ -24,39 +37,44 @@ let elem = 0;
 //Variable to store the number of correct responses
 let correctAnswersCount = 0;
 
+//variable to store user choice for and object that contains words to learn
+let userChoice;
+
 //Initial definition
-function init(){
-    shuffleWords(wordsToLearn);
+function init(array){
+    shuffleWords(array);
+    userChoice = array;
     inputLine.setAttribute("style","visibility: none");
+    respond1.setAttribute("style","visibility: hidden");
     play(elem);
 }
 
 //Function that asks questions
 function play(elem){
         question.innerHTML = "Как будет на английском слово \"" + shuffledWords[elem] + "\"?";
-        respond.innerHTML = "Check";
+        respond.innerHTML = "Проверить";
         document.getElementById("answer").focus();
-        respond.setAttribute("onclick", "checkValue(" + elem + ")");
+        respond.setAttribute("onclick", `checkValue("${elem}")`);
 }
 
 //Function that check if the user entered the translation correctly
 function checkValue(elem) {
     result.setAttribute("style", "visibility: none");
     if (document.getElementById('answer').value != "") {
-        if (document.getElementById('answer').value.toLowerCase() === wordsToLearn[shuffledWords[elem]].toLowerCase()) {
+        if (document.getElementById('answer').value.toLowerCase() === userChoice[shuffledWords[elem]].toLowerCase()) {
             result.setAttribute("style", "font-weight: bold; color: green");
-            result.innerHTML = "You are correct!";
+            result.innerHTML = "Это правильный ответ!";
             correctAnswersCount++;
         }
         else {
             result.setAttribute("style", "font-weight: bold; color: red");
-            result.innerHTML = "You are wrong. Correct spelling is \"" + wordsToLearn[shuffledWords[elem]] + "\".";
+            result.innerHTML = "Неправильно. Корректно писать \"" + userChoice[shuffledWords[elem]] + "\".";
         }
-        respond.innerHTML = "Next";
-        respond.setAttribute("onclick", "next(" + elem + ")");
+        respond.innerHTML = "Далее";
+        respond.setAttribute("onclick", `next("${elem}")`);
     }
     else {
-        result.innerHTML = "Please enter something.";
+        result.innerHTML = "Нужно что-то ввести.";
         play(elem);
     }
 }
@@ -69,11 +87,13 @@ function next(elem){
         play(elem);
     }
     else {
-        question.innerHTML = "Игра закончена";
+        question.innerHTML = "Игра закончена!";
         inputLine.setAttribute("style","visibility: hidden");
-        respond.setAttribute("style","visibility: hidden");
         result.innerHTML = "Твои результаты:<br>правильно " + correctAnswersCount +
             "<br>неправильно " + (shuffledWords.length - correctAnswersCount);
+        respond.innerHTML = "Cыграть еще!";
+        respond.setAttribute("onclick", `location.reload()`);
+
     }
 }
 
